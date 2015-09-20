9ac5e4aabdf101b5a00a01bcb85832fd2613f7bc
Luke Carrier <luke@carrier.im>
2015-09-20 16:45:44 +0100

    Minimum viable electron app

---
+++ main.js
@@ -0,0 +1,25 @@
+var app           = require("app"),
+    BrowserWindow = require("browser-window");
+
+require("crash-reporter").start();
+
+var mainWindow;
+
+app.on("window-all-closed", function() {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("ready", function() {
+    mainWindow = new BrowserWindow({
+        width:  800,
+        height: 600
+    });
+
+    mainWindow.loadUrl("file://" + __dirname + "/index.html");
+
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+});
