c05c056d04d1e1c9a1d52f388c0df6e471f26e49
Luke Carrier <luke@carrier.im>
2015-09-20 20:09:16 +0100

    Wire up all them buttons

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var remote        = require("remote"),
-    Menu          = remote.require("menu"),
-    MenuItem      = remote.require("menu-item");
+var fs       = require("fs"),
+    remote   = require("remote"),
+    dialog   = remote.require("dialog"),
+    Menu     = remote.require("menu"),
+    MenuItem = remote.require("menu-item");
 
 var Application = function(rootNode) {
+    this.fileName  = null;
     this.literally = null;
-    this.rootNode = rootNode;
+    this.rootNode  = rootNode;
 };
 
 Application.prototype.run = function() {
@@ -27,17 +30,31 @@ Application.prototype.initMenu = function() {
                     {
                         label: "Open",
                         accelerator: "CmdOrCtrl+O",
-                        click: this.openFile
+                        click: this.openFile.bind(this)
                     },
                     {
                         label: "Save",
                         accelerator: "CmdOrCtrl+S",
-                        click: this.saveFile
+                        click: this.saveFile.bind(this)
                     },
                     {
                         label: "Save As",
                         accelerator: "CmdOrCtrl+Shift+S",
-                        click: this.saveFileAs
+                        click: this.saveFileAs.bind(this)
+                    }
+                ]
+            },
+            {
+                label: "Help",
+                submenu: [
+                    {
+                        label: "Toggle Developer Tools",
+                        accelerator: (process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I"),
+                        click: function(item, focusedWindow) {
+                            if (focusedWindow) {
+                                focusedWindow.toggleDevTools();
+                            }
+                        }
                     }
                 ]
             }
@@ -48,13 +65,44 @@ Application.prototype.initMenu = function() {
 };
 
 Application.prototype.openFile = function() {
-    console.log("Application.openFile() unimplemented");
+    var application = this;
+
+    dialog.showOpenDialog(function(fileNames) {
+        if (fileNames.length < 1 || fileNames[0] === undefined) {
+            return;
+        }
+        var fileName = fileNames[0];
+
+        fs.readFile(fileName, 'utf-8', function(err, snapshot) {
+            snapshot = JSON.parse(snapshot);
+
+            application.fileName = fileName;
+            application.literally.loadSnapshot(snapshot);
+        });
+    });
 };
 
 Application.prototype.saveFile = function() {
-    console.log("Application.saveFile() unimplemented");
+    var snapshot = JSON.stringify(this.literally.getSnapshot());
+
+    if (this.fileName) {
+        fs.writeFile(this.fileName, snapshot);
+    } else {
+        this.saveFileAs();
+    }
 };
 
 Application.prototype.saveFileAs = function() {
-    console.log("Application.saveFileAs() unimplemented");
+    var application = this,
+        snapshot    = JSON.stringify(this.literally.getSnapshot());
+
+    dialog.showSaveDialog(function(fileName) {
+        if (fileName === undefined) {
+            return;
+        }
+
+        application.fileName = fileName;
+
+        fs.writeFile(fileName, snapshot);
+    });
 };
